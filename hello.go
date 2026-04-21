bbb5872f8b478b1d53478c9d9a7c928e5dfc4414
mohamedali6491 <m.ibrahim6491@gmail.com>
2026-04-22 00:44:24 +0300

    editing go file

--- hello.go
+++ hello.go
@@ -5,3 +5,4 @@ import "fmt"
 func main() {
     fmt.Println("Hello, World!")
 }
+// This is a simple Go program that prints "Hello, World!" to the console. To run this program, save it to a file named "hello.go" and execute it using the command `go run hello.go` in your terminal.
